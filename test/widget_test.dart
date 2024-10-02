4517172d122b9f4699279e65d615bbe9e11e423e
S. Karki <sobitkarki7@gmail.com>
2024-10-02 11:18:12 +0545

    minor fixes; recommendations

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:calc/main.dart.default';
+import 'package:calc/old_main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
